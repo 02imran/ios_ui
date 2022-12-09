89b4a9aea14133aed1f596e1e367dca78bef56fb
imran <imran40031@gmail.com>
2022-12-09 18:43:18 +0600

    Initial Commit

---
+++ Swift-Ui-weather/Swift_Ui_weatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_Ui_weatherApp.swift
+//  Swift-Ui-weather
+//
+//  Created by md imran on 9/12/22.
+//
+
+import SwiftUI
+
+@main
+struct Swift_Ui_weatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
